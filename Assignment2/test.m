3a7ef349887bec9a23e6736685f49a59279e2dca
MatthewSilverstein <matpsilver@gmail.com>
2014-07-18 15:20:26 -0400

    Cost Function computes cost, still needs gradient

---
+++ Assignment2/test.m
@@ -0,0 +1,10 @@
+data = load('ex2data1.txt');
+X = data(:, [1, 2]); y = data(:, 3);
+[m, n] = size(X);
+X = [ones(m, 1) X];
+initial_theta = zeros(n + 1, 1);
+theta=initial_theta;
+A=X*theta;
+h=sigmoid(A);
+J=h
+costFunctionReg(initial_theta,X,y,1)
